b5b710601b34503c27c11f86514a52e9be7097f7
Dysnile <1039906249@qq.com>
2018-09-03 04:33:42 +0800

    Initial Commit

---
+++ data/post-data.js
@@ -0,0 +1,24 @@
+var local_data =[
+  {
+    date: "Set 18 2016",
+    title: "正是虾肥蟹壮时",
+    author_img: "/images/avatar/1.png",
+    post_img: "/images/post/crab.png",
+    content: "本文档将带你一步步创建完成一个微信小程序，并可以在手机上体验该小程序的实际效果。这个小程序的首页将会显示欢迎语以及当前用户的微信头像，点击头像",
+    view_num: "112",
+    collect_num: "11",
+  },
+  {
+    date: "Nov 25 2016",
+    title: "正是虾肥蟹壮时",
+    author_img: "/images/avatar/2.png",
+    post_img: "/images/post/bl.png",
+    content: "本文档将带你一步步创建完成一个微信小程序，并可以在手机上体验该小程序的实际效果。这个小程序的首页将会显示欢迎语以及当前用户的微信头像，点击头像",
+    view_num: "112",
+    collect_num: "97",
+  },
+]
+
+module.exports ={
+  postList: local_data
+}
